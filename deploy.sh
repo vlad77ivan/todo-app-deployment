a45122d2e2127495969737b662e6774298d5d1dd
Vlad Ivan <vlad77ivan@gmail.com>
2022-09-26 17:24:11 +0300

    kubernetes with ingress controller

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker compose build
+
+docker save vlad/todo-frontend:local > todo-frontend.tar
+docker save vlad/todo-backend:local > todo-backend.tar
+
+microk8s ctr image import todo-frontend.tar
+microk8s ctr image import todo-backend.tar
+
+microk8s kubectl apply -f '*.yaml'
